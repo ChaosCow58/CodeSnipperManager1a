46af05ed7511464110eb4e8a63fd85b0971b5423
ChaosCow58 <101840562+ChaosCow58@users.noreply.github.com>
2023-12-18 10:13:44 -0600

    Small Changes

--- CodeSnipperManager1a/MVVM/Model/User.cs
+++ CodeSnipperManager1a/MVVM/Model/User.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 #pragma warning disable CS8618
@@ -13,6 +12,7 @@ namespace CodeSnipperManager1a.MVVM.Model
         public string Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
+        public string ProfileImage { get; set; }
         public string Password { get; set; }
 
     }
