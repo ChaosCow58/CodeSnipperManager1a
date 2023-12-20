d3e60afa5433286deca0e12d8e8ac74aa785bd89
ChaosCow58 <101840562+ChaosCow58@users.noreply.github.com>
2023-12-20 09:07:15 -0600

    Started to add Profile

--- CodeSnipperManager1a/MVVM/View/Profile.xaml.cs
+++ CodeSnipperManager1a/MVVM/View/Profile.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CodeSnipperManager1a.Core;
+using CodeSnipperManager1a.MVVM.Model;
+using CodeSnipperManager1a.MVVM.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +22,68 @@ namespace CodeSnipperManager1a.MVVM.View
     /// </summary>
     public partial class Profile : Window
     {
+
+        private UsersViewModel userViewModel;
+        private SnippetDatabaseAccess databaseAccess;
         public Profile()
         {
             InitializeComponent();
+
+            userViewModel = new UsersViewModel();
+            databaseAccess = new SnippetDatabaseAccess();
+
+            DataContext = userViewModel;
+        }
+
+        private async void SetImages() 
+        { 
+            Task<List<User>> userTask = databaseAccess.GetUser(Globals.UserId);
+            List<User> userList = await userTask;
+
+            foreach (User user in userList) 
+            {
+                imMenuProfile.Source = (ImageSource)FindResource(user.ProfileImage);
+            }
+
+            userList.ForEach(u => userViewModel.Items.Add(u));
+        }
+
+        private void Bottom_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Border border = (Border)e.Source;
+
+            border.Background = Brushes.DarkGray;
+        }
+
+        private void Bottom_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Border border = (Border)e.Source;
+
+            border.Background = Brushes.Transparent;
+        }
+
+        private void LogOff_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+           Close();
+           Login loginWindow = new Login();
+           loginWindow.Owner = null;
+           loginWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+           loginWindow.ShowDialog();
+
+           if (Globals.UserId == "") 
+           {
+               App.Current.Shutdown();
+           }
+        }
+
+        private void ExitApp_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void ExitProfile_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Close();
         }
     }
 }
